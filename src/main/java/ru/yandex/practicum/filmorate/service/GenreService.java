f44574d6a8edc586a59d09281d71f0ff0147c3c1
maksy029 <maksy029@yandex.ru>
2023-12-08 20:07:36 +0300

    ТЗ11 - добавлены недостающие log в сервисах

--- src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Genre;
@@ -8,6 +9,7 @@ import ru.yandex.practicum.filmorate.storage.GenreStorage;
 import java.util.Collection;
 
 @Service
+@Slf4j
 public class GenreService {
 
     private final GenreStorage genreStorage;
@@ -18,10 +20,12 @@ public class GenreService {
     }
 
     public Collection<Genre> findAll() {
+        log.debug("Получен запрос на получение списка всех жанров");
         return genreStorage.findAll();
     }
 
     public Genre genreById(Integer genreId) {
+        log.debug("Получен запрос на получение жанра с id=" + genreId);
         return genreStorage.genreById(genreId);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/MpaService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/MpaService.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Mpa;
@@ -8,6 +9,7 @@ import ru.yandex.practicum.filmorate.storage.MpaStorage;
 import java.util.Collection;
 
 @Service
+@Slf4j
 public class MpaService {
     private final MpaStorage mpaStorage;
 
@@ -17,10 +19,12 @@ public class MpaService {
     }
 
     public Collection<Mpa> findAll() {
+        log.debug("Получен запрос на получение списка всех рейтингов");
         return mpaStorage.findAll();
     }
 
     public Mpa mpaById(Integer mpaId) {
+        log.debug("Получен запрос на получение рейтинга с id=" + mpaId);
         return mpaStorage.mpaById(mpaId);
     }
 }
